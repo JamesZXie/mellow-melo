1b690766eca816ea69ca0329a25b1a67cde2acae
James Xie <james.xie@ibm.com>
2019-08-30 10:17:09 -0500

    basic cashier-view setup

---
+++ client/src/components/cashier-view/Header/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <span>
+            Mellow Melo's
+        </span>
+        <span>
+            Cashier ID
+        </span>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.container {
+    background-color: red;
+}
+
+</style>
---
+++ client/src/components/test/test.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1 @click="increment">{{ counter }}</h1>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            hello: "hello",
+        }
+    },
+    computed: {
+        counter() {
+            return this.$store.state.counter;
+        }
+    },
+    methods: {
+        increment() {
+            this.$store.state.counter++;
+        }
+    }
+}
+
+</script>
